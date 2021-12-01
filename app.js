1c7423bf0c6a9991ed64ce2042ce52ef0f811b2c
SAM MELVIN <imsammelvin>
2021-12-01 23:34:59 +0530

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -118,6 +118,11 @@ app.post('/subscribe', (req,res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server in port 5000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5000;
+}
+
+app.listen(port, () => {
+  console.log("Server has started successfully");
 });
